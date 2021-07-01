71fd57c27400ea3bf352e64be58b5ab9d795b4d2
Hammertonmutuku <lthamma3@gmail.com>
2021-07-01 14:12:15 +0300

    https

--- config/app.php
+++ config/app.php
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => 'https://msaada254.herokuapp.com/',
+    'url' => env('APP_URL', 'https://msaada254.herokuapp.com/'),
 
     /*
     |--------------------------------------------------------------------------
